d88f46d6dd7de7b8da2534184ca44f7bafbff2ac
Easton Bolinger <bolinger@ksu.edu>
2022-02-15 19:49:43 -0600

    new project started

---
+++ GameProject0/Sprites/ChickenSprite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace GameProject0
+{
+    public class ChickenSprite
+    {
+        private Texture2D texture;
+        private Vector2 position = new Vector2(200, 200);
+
+        public void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("Chicken");
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+            
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+
+    }
+}
---
+++ GameProject0/Sprites/SnakeSprite.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject0.Sprites
+{
+    class SnakeSprite
+    {
+    }
+}
